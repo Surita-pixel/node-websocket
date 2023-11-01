00ad3da1f746f13a8a3bce11aea63021f87fe1ec
surita <suritabtc@gmail.com>
2023-10-31 20:24:03 -0500

    first commit

---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import logger from "morgan";
+
+import {Server} from 'socket.io'
+import {createServer} from 'node:http'
+import { Socket } from "node:dgram";
+
+
+const port = process.env.PORT ?? 3000
+
+const app = express()
+const server = createServer(app)
+
+const io = new Server(server, {connectionStateRecovery:{}})
+
+
+io.on('connection', (socket)=>{
+    console.log('a user has connected!')
+    socket.on('chat message', (msg)=>{
+        io.emit('chat message', (msg))
+    })
+})
+
+app.use(logger('dev'))
+app.get('/', (req, res) =>{
+    res.sendFile(process.cwd()+'/client/index.html')
+})
+
+server.listen(port, ()=>{ 
+    console.log(`Server running on port ${port}`)
+})
